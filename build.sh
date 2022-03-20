4d36e280a9e8cc74836fb2603916fb48a3bfaabc
Marcythm <marcythm@gmail.com>
2022-03-20 19:00:17 +0800

    add framework for problem 4

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# run AVD with kernel supporting module by:
+# `android-sdk-linux/tools/emulator -avd <avdName> -kernel kernel/goldfish/arch/arm/boot/zImage -show-kernel`
+
 # build ptree syscall and push onto adb (problem 1)
 cd problem1
 make
@@ -10,11 +13,11 @@ cd ..
 # build test for ptree syscall and push onto adb (problem 2)
 cd problem2/jni
 ndk-build && adb push ../libs/armeabi/ptreeTestARM /data/misc/ptreeTestARM
-rm -rf ../libs
+rm -rf ../libs ../obj
 cd ../..
 
 # build genProcess and push onto adb (problem 3)
 cd problem3/jni
 ndk-build && adb push ../libs/armeabi/genProcessARM /data/misc/genProcessARM
-rm -rf ../libs
+rm -rf ../libs ../obj
 cd ../..
